ec991d3b9cead6588a6237788574d4534b8be248
kyoungminlee <kyoungminlee32@gmail.com>
2026-01-30 15:27:23 +0900

    function  추가 수정

--- function.js
+++ function.js
@@ -9,3 +9,29 @@ function log(message) {
 }
 log("안녕하세요");
 log("감사");
+
+const ellie = {name: "ellie"};
+
+function changeName(obj) {
+    obj.name = "coder";
+}
+
+changeName(ellie);
+console.log(ellie);
+console.log("===========================");
+function showMessage(message, from = "unknown") {
+    console.log(`${message} by ${from}`);
+}
+showMessage("hi");
+showMessage("hi", "ellie");
+console.log("===========================");
+function printAll(...args) {
+    for(let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+    console.log("===========================");
+    for (const argsItem of args) {
+        console.log(argsItem);
+    }
+}
+printAll("dream", "coding");
