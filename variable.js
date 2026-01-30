461d7d9ca1242fdcdda236e37c84874c017b54fa
kyoungminlee <kyoungminlee32@gmail.com>
2026-01-30 13:36:52 +0900

    variable js 완료

---
+++ variable.js
@@ -0,0 +1,10 @@
+"use strict";
+let globalName = "global name";
+{
+    let name = "ellie";
+    console.log(name);
+    name = "hellow";
+    console.log(name);
+    console.log(globalName);
+    
+}
